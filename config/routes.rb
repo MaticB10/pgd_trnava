4224766a981b513cc6052c339db88d2cd8e7afda
MaticB <87810500+MaticB10@users.noreply.github.com>
2025-03-31 13:29:00 +0200

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
-  root "home#index"
+    root "home#index"
   
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+    devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  
+    # Admin panel za novice, intervencije in kategorije galerije
+    namespace :admin do
+      resources :novicas      # Novice
+      resources :intervencijas # Intervencije
+      resources :gallery_categories  # Kategorije slik
+    end
+  
+    # Funkcionalnost za redne uporabnike – nalaganje slik v galerijo
+    resources :galerijas, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
   # Kasneje bomo dodali še routanje za funkcionalnosti, npr. novice, intervencije, galerije
 
