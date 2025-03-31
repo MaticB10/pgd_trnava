f3627aa456a4c06de6fc61dfdd2fa339e02ecad3
MaticB <87810500+MaticB10@users.noreply.github.com>
2025-03-31 14:02:13 +0200

    init

--- app/controllers/galerijas_controller.rb
+++ app/controllers/galerijas_controller.rb
@@ -1,6 +1,13 @@
 class GalerijasController < ApplicationController
+  # Samo prijavljeni uporabniki lahko ustvarjajo in urejajo galerije
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_galerija, only: [:show, :edit, :update, :destroy]
+
   def index
-    @galerijas = Galerija.all
+    @galerijas = Galerija.all.order(created_at: :desc)
+  end
+
+  def show
   end
 
   def new
@@ -10,17 +17,35 @@ class GalerijasController < ApplicationController
   def create
     @galerija = Galerija.new(galerija_params)
     if @galerija.save
-      redirect_to galerija_path(@galerija), notice: "Slike so bile uspešno naložene."
+      redirect_to @galerija, notice: "Galerija je bila uspešno ustvarjena."
     else
       render :new
     end
   end
 
-  # Dodajte še ostale akcije: index, show, edit, update, destroy
+  def edit
+  end
+
+  def update
+    if @galerija.update(galerija_params)
+      redirect_to @galerija, notice: "Galerija je bila uspešno posodobljena."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @galerija.destroy
+    redirect_to galerijas_path, notice: "Galerija je bila uspešno izbrisana."
+  end
 
   private
 
+  def set_galerija
+    @galerija = Galerija.find(params[:id])
+  end
+
   def galerija_params
-    params.require(:galerija).permit(:gallery_category_id, :description, images: [])
+    params.require(:galerija).permit(:description, images: [])
   end
 end
--- app/models/galerija.rb
+++ app/models/galerija.rb
@@ -1,4 +1,4 @@
+# app/models/galerija.rb
 class Galerija < ApplicationRecord
-  belongs_to :gallery_category
   has_many_attached :images
 end
