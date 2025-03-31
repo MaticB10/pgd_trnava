5f9798a29ce0ebdd0fc9b38bb48ceb7b25ea5adc
MaticB <87810500+MaticB10@users.noreply.github.com>
2025-03-31 11:07:56 +0200

    init

--- db/migrate/20250331085613_create_galerijas.rb
+++ db/migrate/20250331085613_create_galerijas.rb
@@ -1,9 +1,11 @@
 class CreateGalerijas < ActiveRecord::Migration[8.0]
   def change
-    # create_table :galerijas do |t|
-    #   t.bigint :gallery_category_id
+    unless table_exists?(:galerijas)
+      create_table :galerijas do |t|
+        t.bigint :gallery_category_id
 
-    #   t.timestamps
-    # end
+        t.timestamps
+      end
+    end
   end
 end
