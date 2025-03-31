af20d0fa5bbb2ebed58245cce4d893e9be160076
MaticB <87810500+MaticB10@users.noreply.github.com>
2025-03-31 14:12:35 +0200

    init

--- app/models/galerija.rb
+++ app/models/galerija.rb
@@ -1,4 +1,12 @@
-# app/models/galerija.rb
 class Galerija < ApplicationRecord
+  belongs_to :gallery_category
   has_many_attached :images
+
+  before_validation :set_default_gallery_category, on: :create
+
+  private
+
+  def set_default_gallery_category
+    self.gallery_category_id ||= 1
+  end
 end
