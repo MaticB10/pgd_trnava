42130869b4e24337049d70d0cebc3f8656150cd1
MaticB <87810500+MaticB10@users.noreply.github.com>
2025-03-31 11:00:32 +0200

    init

--- db/migrate/20250331085556_create_novicas.rb
+++ db/migrate/20250331085556_create_novicas.rb
@@ -1,11 +1,11 @@
 class CreateNovicas < ActiveRecord::Migration[8.0]
   def change
-    create_table :novicas do |t|
-      t.string :naslov
-      t.text :vsebina
-      t.datetime :datum
+    # create_table :novicas do |t|
+    #   t.string :naslov
+    #   t.text :vsebina
+    #   t.datetime :datum
 
-      t.timestamps
-    end
+    #   t.timestamps
+    # end
   end
 end
