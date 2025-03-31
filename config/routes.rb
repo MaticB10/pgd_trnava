76b39c5996f4be973115a7bd257a3b821292da25
MaticB <87810500+MaticB10@users.noreply.github.com>
2025-03-31 10:53:46 +0200

    init

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "home#index"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  # Kasneje bomo dodali še routanje za funkcionalnosti, npr. novice, intervencije, galerije
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # Primer:
+  resources :novicas
+  resources :intervencijas
+  resources :galerijas
 end
