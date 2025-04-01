bbe75492dae0d63d1dbe8d81c6acd5bd67c65feb
MaticB <87810500+MaticB10@users.noreply.github.com>
2025-04-01 09:28:43 +0200

    init 1

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_controller.forgery_protection_origin_check = false
   config.hosts += [
     "pgd-trnava.eu",
     "www.pgd-trnava.eu",
