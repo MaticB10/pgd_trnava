35bac4014195af622f27ba3ad0a3b7ef264e7818
MaticB <87810500+MaticB10@users.noreply.github.com>
2025-04-01 09:22:44 +0200

    zagon 6

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,7 +2,12 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.hosts += [
+    "pgd-trnava.eu",
+    "www.pgd-trnava.eu",
+    /.*\.pgd-trnava\.eu/
+  ]
+  
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
 
