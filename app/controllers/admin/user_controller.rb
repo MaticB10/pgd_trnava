bc0f197da1f6db07fcccf675b784c1223bfedb28
MaticB <87810500+MaticB10@users.noreply.github.com>
2025-04-01 08:40:52 +0200

    init

--- app/controllers/admin/user_controller.rb
+++ app/controllers/admin/user_controller.rb
@@ -1,23 +1,25 @@
 class Admin::UsersController < ApplicationController
-    before_action :authenticate_user!
-    before_action :require_admin
-  
-    def approve
-      user = User.find(params[:id])
-      user.update(approved: 1)
-      redirect_to admin_dashboard_index_path, notice: "#{user.username} has been approved."
-    end
-  
-    def reject
-      user = User.find(params[:id])
-      user.destroy
-      redirect_to admin_dashboard_index_path, notice: "#{user.username} has been rejected and removed."
-    end
-  
-    private
-  
-    def require_admin
-      redirect_to root_path, alert: "Dostop ni dovoljen." unless current_user.username == "admin"
+  before_action :set_user, only: [:approve, :reject]
+
+  def approve
+    if @user.update(approved: 1)
+      redirect_to admin_dashboard_index_path, notice: "Uporabnik je bil uspešno potrjen."
+    else
+      redirect_to admin_dashboard_index_path, alert: "Pri potrjevanju uporabnika je prišlo do napake."
     end
   end
-  
+
+  def reject
+    if @user.update(approved: 0)
+      redirect_to admin_dashboard_index_path, notice: "Uporabnik je bil zavrnjen."
+    else
+      redirect_to admin_dashboard_index_path, alert: "Pri zavrnitvi uporabnika je prišlo do napake."
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+end
