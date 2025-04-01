ae679c5befb72b9df54f98f87a64095f5fff7aa9
MaticB <87810500+MaticB10@users.noreply.github.com>
2025-04-01 09:07:10 +0200

    init

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,10 +80,12 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  config.hosts = [
-    "pgd-trnava.eu",     # Allow requests from example.com
-    /.*\.pgd-trnava\.eu/ # Allow requests from subdomains like `www.example.com`
+  config.hosts += [
+    "pgd-trnava.eu",
+    "www.pgd-trnava.eu",
+    /.*\.pgd-trnava\.eu/
   ]
+  
   #
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
